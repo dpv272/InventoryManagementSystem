8d9309b8248cd6fbe70c96359504a52e733c0f3c
DP <wiedzmin2702@gmail.com>
2023-12-13 19:47:45 +0100

    Add Custom Button

---
+++ CustomButton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace InventoryManagementSystem
+{
+    public partial class CustomButton : PictureBox
+    {
+        public CustomButton()
+        {
+            InitializeComponent();
+        }
+
+        private Image img;
+        private Image imgHover;
+
+        public Image Img
+        {
+            get { return img; }
+            set { img = value; }
+        }
+
+        public Image ImgHover
+        {
+            get { return imgHover; }
+            set { imgHover = value; }
+        }
+
+        private void CustomButton_MouseHover(object sender, EventArgs e)
+        {
+            this.Image = imgHover;
+        }
+
+        private void CustomButton_MouseLeave(object sender, EventArgs e)
+        {
+            this.Image = img;
+        }
+    }
+}
